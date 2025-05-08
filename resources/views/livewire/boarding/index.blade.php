8f3896c2c0ef14aa19be14b2fddf0e748afd6a8a
naudefirst <naudes@gmail.com>
2025-05-08 17:14:31 +1000

    Add new Popups

--- resources/views/livewire/boarding/index.blade.php
+++ resources/views/livewire/boarding/index.blade.php
@@ -1,44 +1,69 @@
 @php use App\Enums\ProxyTypes; @endphp
 <x-slot:title>
-    Onboarding | Coolify
-</x-slot>
+    <div class="flex items-center space-x-4">
+        <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-Portals-Banner.png" alt="AirDrive Logo" style="width: 100%; max-width: 300px; height: auto;"><br>        <span>Onboarding | AirDrive Cloud</span>
+    </div>
+</x-slot:title>
 <section class="flex flex-col h-full lg:items-center lg:justify-center">
     <div
         class="flex flex-col items-center justify-center p-10 mx-2 mt-10 bg-white border rounded-lg shadow lg:p-20 dark:bg-transparent dark:border-none max-w-7xl ">
         @if ($currentState === 'welcome')
-            <h1 class="text-3xl font-bold lg:text-5xl">Welcome to Coolify</h1>
-            <div class="py-6 text-center lg:text-xl">Let me help you set up the basics.</div>
+            
+            <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-Portals-Banner.png" alt="AirDrive Logo" style="width: 100%; max-width: 300px; height: auto;"><br></a>
+            <div class="py-6 text-center lg:text-xl">Let me help you set up your AirDrive.one or AirDrive.vps Server</div>
             <div class="flex justify-center ">
                 <x-forms.button class="justify-center w-64 box-boarding"
                     wire:click="$set('currentState','explanation')">Get
                     Started
+                
                 </x-forms.button>
+               
             </div>
+             <br><br>
+             <p> If you do not have an AirDrive.one or Airdrive.VPS Server then please follow the links below
+             </p><br><br>
+            <div style="display: flex; gap: 20px; justify-content: center;">
+             <a href="https://airdrive.one" target="_blank"
+             style="padding: 12px 24px; font-weight: 600; background-color: white; border: 1px solid #ddd; border-radius: 8px; text-decoration: none; color: #000; transition: all 0.2s ease;">
+             AirDrive.one
+            </a>
+
+            <a href="https://airdrive.vps" target="_blank"
+             style="padding: 12px 24px; font-weight: 600; background-color: white; border: 1px solid #ddd; border-radius: 8px; text-decoration: none; color: #000; transition: all 0.2s ease;">
+             AirDrive.vps
+            </a>
+           </div>
+                <br>
         @elseif ($currentState === 'explanation')
-            <x-boarding-step title="What is Coolify?">
+       <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-Portals-Banner.png" alt="AirDrive Logo" style="width: 100%; max-width: 300px; height: auto;"><br></a><br>
+
+            <x-boarding-step title="What is AirDrive Cloud?">
                 <x-slot:question>
-                    Coolify is an all-in-one application to automate tasks on your servers, deploy applications with
+                    AirDrive Cloud is an all-in-one application to automate tasks on your servers, deploy applications with
                     Git
                     integrations, deploy databases and services, monitor these resources with notifications and
                     alerts
                     without vendor lock-in. <br/>
-                <a href="https://coolify.io" class="dark:text-white hover:underline">Coolify Home</a>.
+                
                     <br><br>
-                    <span class="text-xl">
-                        <x-highlighted text="Self-hosting with superpowers!" /></span>
+                      <span style="color: #ff9400; font-weight: bold;">The Cloud for your Home &amp; Business</span>
                 </x-slot:question>
                 <x-slot:explanation>
                     <p>
-                        <x-highlighted text="Task automation:" /> You don't need to manage your servers anymore.
+                        <span style="color: #ff9400; font-weight: bold;">Task Automation</span> You don't need to manage your servers anymore.
                         Coolify does
                         it for you.
                     </p>
                     <p>
-                        <x-highlighted text="No vendor lock-in:" /> All configurations are stored on your servers, so
+                        <span style="color: #ff9400; font-weight: bold;">No Vendor Lock-in</span> All configurations are stored on your servers, so
                         everything works without a connection to Coolify (except integrations and automations).
                     </p>
                     <p>
-                        <x-highlighted text="Monitoring:" />You can get notified on your favourite platforms
+                        <span style="color: #ff9400; font-weight: bold;">Huge Selection of Apps</span> We have a huge selection of Application to choose from.
+                    </p>
+
+                    <p>
+                        <span style="color: #ff9400; font-weight: bold;">Monitoring</span> You can get notified on your favourite platforms
                         (Discord,
                         Telegram, Email, etc.) when something goes wrong, or an action is needed from your side.
                     </p>
@@ -49,21 +74,27 @@
                 </x-slot:actions>
             </x-boarding-step>
         @elseif ($currentState === 'select-server-type')
-            <x-boarding-step title="Server">
+              <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-Portals-Banner.png" alt="AirDrive Logo" style="width: 100%; max-width: 300px; height: auto;"><br></a><br><br>
+            <x-boarding-step title="Add your AirDrive or VPServer">
+             
                 <x-slot:question>
-                    Do you want to deploy your resources to your
-                    <x-highlighted text="Localhost" />
-                    or to a
-                    <x-highlighted text="Remote Server" />?
+                    <span style="color: #ff9400; font-weight: bold;">Deploy your AirDrive.one Server now</span>                   
                 </x-slot:question>
                 <x-slot:actions>
-                    <x-forms.button class="justify-center w-64 box-boarding" wire:target="setServerType('localhost')"
-                        wire:click="setServerType('localhost')">Localhost
-                    </x-forms.button>
+                   
 
                     <x-forms.button class="justify-center w-64 box-boarding " wire:target="setServerType('remote')"
-                        wire:click="setServerType('remote')">Remote Server
+                        wire:click="setServerType('remote')">Deploy your Server Now
                     </x-forms.button>
+                    <div class="inline-flex items-center space-x-2">
+                        <a href="https://103.191.36.2:4083" target="_blank"
+                           class="justify-center w-64 box-boarding inline-flex items-center px-4 py-2 font-semibold text-white transition-colors duration-300 rounded"
+                           style="background-color: #;">
+                           Create a New AirDrive.VPS Server
+                        </a>
+                       
+                   </div> 
+
 
                     @if (!$serverReachable)
                         <div class="mt-6 p-4 border border-error rounded-lg text-gray-800 dark:text-gray-200">
@@ -111,22 +142,47 @@
                     @endif
                 </x-slot:actions>
                 <x-slot:explanation>
-                    <p>Servers are the main building blocks, as they will host your applications, databases,
+                    <p> <span style="color: #ff9400; font-weight: bold;">AirDrive.one Servers: </span> the main building blocks, as they will host your applications, databases,
                         services, called resources. Any CPU intensive process will use the server's CPU where you
                         are deploying your resources.</p>
                     <p>
-                        <x-highlighted text="Localhost" /> is the server where Coolify is running on. It is not
-                        recommended to use one server
-                        for everything.
+                        <span style="color: #ff9400; font-weight: bold;">AirDrive.one & AirDrive.VPS:</span>  is the server where all your Documents, Photos, Videos and Applications are being kept
+                        we have no access to it unless you give us explicit access for Support Functions you might need, the access can be revoked immediately after
+                        we are done with the Support Request by you.
                     </p>
                     <p>
-                        <x-highlighted text="A remote server" /> is a server reachable through SSH. It can be hosted
-                        at home, or from any cloud
-                        provider.
+                        <span style="color: #ff9400; font-weight: bold;">What Devices can join?: </span> The AirDrive Cloud Platform can accept a Physical Computer, Virtual Server or Rapspberry Pi can be added to the AirDrive Cloud Network
+                        as long as it has an Ubuntu Server Operating System with LXD pre-installed. 
                     </p>
-                </x-slot:explanation>
+                        <p>
+                        <span style="color: #ff9400; font-weight: bold;">Can I add a AWS or Azure Server? : </span> Yes, as long as it meets the requirements as set out in the paragraph above
+                    </p>
+                        </p>
+                        <p>
+                        <span style="color: #ff9400; font-weight: bold;">AirDrive Cloud VPS : </span> By clicking the button below you can instantly create a VPS
+                        that can be added in this section for immediate rollout of your Private Cloud. Just follow the instructions on screen on in a few minutes you
+                        will have access to a huge library of Software that are all 100% free to use.
+                    </p>
+
+
+                    <div class="inline-flex items-center space-x-2">
+                        <a href="https://103.191.36.2:4083" target="_blank"
+                           class="justify-center w-64 box-boarding inline-flex items-center px-4 py-2 font-semibold text-white transition-colors duration-300 rounded"
+                           style="background-color: #;">
+                           Create a New VPS
+                        </a>
+                        <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-nNetwork-Banner.png" alt="icon" style="width: 200px; height: px;" />
+                   </div> 
+                    <p>
+                        <span style="color: #ff9400; font-weight: bold;">Other VPS providers: </span> You can add any VPS from any provider such as AWS, Google and Azure, if the 
+                        VPS meets all requirements - We do not offer any Support any VPS's that is not inside the AirDrive Cloud Network.
+                    </p>
+           
+            </x-slot:explanation>
             </x-boarding-step>
         @elseif ($currentState === 'private-key')
+                 <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-Portals-Banner.png" alt="AirDrive Logo" style="width: 100%; max-width: 300px; height: auto;"><br></a><br>
+
             <x-boarding-step title="SSH Key">
                 <x-slot:question>
                     Do you have your own SSH Private Key?
@@ -152,11 +208,34 @@
                 </x-slot:actions>
                 <x-slot:explanation>
                     <p>SSH Keys are used to connect to a remote server through a secure shell, called SSH.</p>
-                    <p>You can use your own ssh private key, or you can let Coolify to create one for you.</p>
+                    <p>You can use your own ssh private key, or you can let AirDrive Cloud Portal to create one for you.</p>
                     <p>In both ways, you need to add the public version of your ssh private key to the remote
                         server's
                         <code class="dark:text-warning">~/.ssh/authorized_keys</code> file.
                     </p>
+                    <p> <span style="color: #ff9400; font-weight: bold;">Have You being Invited to Join a Team: </span> If you have an invitation link send to you by an Existing AirDrive Cloud
+                        Network, then you can choose from the available SSH keys that was made availiable to you by the existing AirDrive Cloud Network, then
+                        you can advance to the next step by clicking on the "Use this SSH Key"</p>
+                    <p>
+                        <span style="color: #ff9400; font-weight: bold;">If you have just Aquired an AirDrive.one Server:</span>  The you can select one of the two options, If you
+                        have an existing SSH Certificate then you can add that to your AirDrive.one or AirDrive.VPS Server, or you can let AirDrive portal create one for you. Make
+                        of both of the Certificate and store  them in a safe Place. You will need the Public Key to add to your Server and the Private Key is kept safe inside your
+                        AirDrive Cloud Portal Profile.
+                    </p>
+                    <p>
+                        <span style="color: #ff9400; font-weight: bold;">Why do you need an SSH Certificate?: </span> A SSH Certificate is used for the encryption of add data that 
+                        are being sent to and from your AirDRine.one or AirDrive.VPS Server and your Home or Buniness Device. There is no need to enter a User or Password every
+                        time you are using your Private Cloud.
+                     </p>
+                   
+
+
+                    
+                    <p>
+                        <span style="color: #ff9400; font-weight: bold;">Other VPS providers: </span> You can add any VPS from any provider such as AWS, Google and Azure, if the 
+                        VPS meets all requirements - We do not offer any Support any VPS's that is not inside the AirDrive Cloud Network.
+                    </p>
+
                 </x-slot:explanation>
             </x-boarding-step>
         @elseif ($currentState === 'select-existing-server')
@@ -237,9 +316,12 @@
                 </x-slot:explanation>
             </x-boarding-step>
         @elseif ($currentState === 'create-private-key')
+ <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-Portals-Banner.png" alt="AirDrive Logo" style="width: 100%; max-width: 300px; height: auto;"><br></a><br>
             <x-boarding-step title="Create Private Key">
                 <x-slot:question>
-                    Please let me know your key details.
+               
+                    Paste you Private Key into the Input Box provided if you do not have an SSH key the follow the link at the Bottom
+                    Right and create one or go Back and let AirDrive Cloud Portal create one for you.
                 </x-slot:question>
                 <x-slot:actions>
                     <form wire:submit='savePrivateKey' class="flex flex-col w-full gap-4 lg:pr-10">
@@ -261,16 +343,20 @@
                 </x-slot:actions>
                 <x-slot:explanation>
                     <p>Private Keys are used to connect to a remote server through a secure shell, called SSH.</p>
-                    <p>You can use your own private key, or you can let Coolify to create one for you.</p>
-                    <p>In both ways, you need to add the public version of your private key to the remote server's
+                    <p>You can use your own private key, or you can let AirDrive Cloud Portal create one for you.</p>
+                    <p>Either way, you need to add the Public Key of your Private Key to your AirDrive.one or AirDrive.VPS's
+                       Users root directory as specified below.
+                    <br>
                         <code>~/.ssh/authorized_keys</code> file.
                     </p>
+                  
                 </x-slot:explanation>
             </x-boarding-step>
         @elseif ($currentState === 'create-server')
+          <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-Portals-Banner.png" alt="AirDrive Logo" style="width: 100%; max-width: 300px; height: auto;"><br></a><br>
             <x-boarding-step title="Create Server">
                 <x-slot:question>
-                    Please let me know your server details.
+                    Enter your AirDrive.one or AirDrive.VPS server details below.
                 </x-slot:question>
                 <x-slot:actions>
                     <form wire:submit='saveServer' class="flex flex-col w-full gap-4 lg:w-96">
@@ -301,9 +387,20 @@
                         </div>
                         <x-forms.button type="submit">Continue</x-forms.button>
                     </form>
+                
                 </x-slot:actions>
+               <x-slot:explanation>
+                 <p> <span style="color: #ff9400; font-weight: bold;">Add your Servers: </span> Your AiDrive.one device is the main 
+                    first building block to create your own Pivate AirDrive Cloud
+                     
+                    Enter the IP address of the Server you want to add
+                       </p>
+                   
+           
+              </x-slot:explanation>        
             </x-boarding-step>
-        @elseif ($currentState === 'validate-server')
+             
+         @elseif ($currentState === 'validate-server')
             <x-boarding-step title="Validate & Configure Server">
                 <x-slot:question>
                     I need to validate your server (connection, Docker Engine, etc) and configure if something is
