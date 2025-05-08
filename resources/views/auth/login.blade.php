b8404f6eb07e5e8710b857e411d69c9401b87370
naudefirst <naudes@gmail.com>
2025-05-08 11:22:23 +1000

    Restoring my changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <section class="bg-gray-50 dark:bg-base">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <a class="flex items-center mb-6 text-5xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-                Coolify
+                AirDrive
             </a>
             <div class="w-full bg-white shadow md:mt-0 sm:max-w-md xl:p-0 dark:bg-base ">
                 @if ($errors->any())
