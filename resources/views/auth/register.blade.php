966ac33c82a777ddab40226b4f46d9baaceaca21
naudefirst <naudes@gmail.com>
2025-05-08 14:51:43 +1000

    Your message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,17 @@
-<?php
-function getOldOrLocal($key, $localValue)
-{
-    return old($key) != '' ? old($key) : (app()->environment('local') ? $localValue : '');
-}
-
-$name = getOldOrLocal('name', 'test3 normal user');
-$email = getOldOrLocal('email', 'test3@example.com');
-?>
-
 <x-layout-simple>
     <section class="bg-gray-50 dark:bg-base">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/air-drive-cloud-banner.png" alt="AirDrive Logo" class="w-[200px] mr-4" />
-</div>
+            <img src="https://s3.ap-southeast-2.wasabisys.com/airdrive-coolify/AirDrive-Cloud-Portals-Banner.png" alt="AirDrive Logo" class="w-[200px] mr-4" />
+
             <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 dark:bg-base">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <div>
-                        <h1
-                            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Create an account
                         </h1>
                         @if ($isFirstUser)
-                            <div class="text-xs dark:text-warning">This user will be the root user (full admin access).
+                            <div class="text-xs dark:text-warning">
+                                This user will be the root user (full admin access).
                             </div>
                         @endif
                     </div>
@@ -35,8 +25,10 @@ $email = getOldOrLocal('email', 'test3@example.com');
                             label="{{ __('input.password') }}" />
                         <x-forms.input id="password_confirmation" required type="password" name="password_confirmation"
                             label="{{ __('input.password.again') }}" />
-                        <div class="text-xs w-full">Your password should be min 8 characters long and contain
-                            at least one uppercase letter, one lowercase letter, one number, and one symbol.</div>
+                        <div class="text-xs w-full">
+                            Your password should be min 8 characters long and contain
+                            at least one uppercase letter, one lowercase letter, one number, and one symbol.
+                        </div>
                         <div class="flex flex-col gap-4 pt-8 w-full">
                             <x-forms.button class="w-full" type="submit">Register</x-forms.button>
                             <a href="/login" class="w-full text-xs">
